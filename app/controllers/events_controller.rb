cc3d47f3627a2a0c24987eb4770c90e97832c176
amalrik <amalrik.maia@gmail.com>
2016-09-25 18:15:18 -0300

    create events with ajax

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,28 @@
 class EventsController < ApplicationController
 
   def new
-    @event = Event.new
+    @dashboard = current_user.dashboard
+    @event = @dashboard.events.build
     respond_to do |format|
       format.js {render 'new.js.erb'}
     end
   end
 
   def create
-    byebug
+    dashboard = Dashboard.where(id: params[:event][:dashboard_id]).take
+    event = dashboard.events.build event_params
+
+    respond_to do |format|
+      if event.save
+        format.js { render 'create.js.erb' }
+      else
+        format.js { render 'create_error.js.erb' }
+      end
+    end
   end
 
   private
   def event_params
-    params.require(:event).permit(:date, :kind, :amount, )
+    params.require(:event).permit(:date, :kind, :amount)
   end
 end
